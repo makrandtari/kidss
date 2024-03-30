5f9d16d552a2abc2d994ce4954529cbf9c6f31bf
makrandtari <109663993+makrandtari@users.noreply.github.com>
2024-03-30 18:51:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+function apple(letter) {
+    var sound = document.getElementById("speaker");
+
+    if (letter == 'a') sound.src = "sound/apple.mp3";
+    else if (letter == 'b') sound.src = "sound/ball.mp3";
+    else if (letter == 'c') sound.src = "sound/cat.mp3";
+    else if (letter == 'd') sound.src = "sound/d.mp3";
+
+    else if (letter == 'e') sound.src = "sound/e.mp3";
+    else if (letter == 'f') sound.src = "sound/f.mp3";
+    else if (letter == 'g') sound.src = "sound/g.mp3";
+    else if (letter == 'h') sound.src = "sound/h.mp3";
+
+    else if (letter == 'i') sound.src = "sound/i.mp3";
+    else if (letter == 'j') sound.src = "sound/j.mp3";
+    else if (letter == 'k') sound.src = "sound/k.mp3";
+    else if (letter == 'l') sound.src = "sound/l.mp3";
+
+    else if (letter == 'm') sound.src = "sound/m.mp3";
+    else if (letter == 'n') sound.src = "sound/n.mp3";
+    else if (letter == 'o') sound.src = "sound/o.mp3";
+    else if (letter == 'p') sound.src = "sound/p.mp3";
+
+    else if (letter == 'q') sound.src = "sound/q.mp3";
+    else if (letter == 'r') sound.src = "sound/r.mp3";
+    else if (letter == 's') sound.src = "sound/s.mp3";
+    else if (letter == 't') sound.src = "sound/t.mp3";
+
+    else if (letter == 'u') sound.src = "sound/u.mp3";
+    else if (letter == 'v') sound.src = "sound/v.mp3";
+    else if (letter == 'w') sound.src = "sound/w.mp3";
+    else if (letter == 'x') sound.src = "sound/x.mp3";
+
+    else if (letter == 'y') sound.src = "sound/y.mp3";
+    else if (letter == 'z') sound.src = "sound/z.mp3";
+
+    sound.play();
+
+
+
+
+}
+
